67ad0e2832c60af0dc7560c4dc7f77c20c4732d8
Mateusz Lerczak <mlerczak@sii.pl>
2020-01-21 20:39:46 +0100

    Wstęp do dockera - instalacja dockera

---
+++ 001-setup-docker.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+sudo apt update
+
+sudo apt install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+
+    Software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt update
+
+sudo apt install docker-ce docker-ce-cli
+
+docker -v
